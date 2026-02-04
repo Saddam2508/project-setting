02782e1b23625653afc0900725ae2d209843351e
Saddam <eyarinsultana@gmail.com>
2026-02-04 19:43:37 +0600

    route set

---
+++ client/src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { FC } from "react";
+
+const page: FC = () => {
+  return <div></div>;
+};
+
+export default page;
---
+++ client/src/views/admin/banner/BannerSetting.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const BannerSetting: FC = () => {
+  return <div></div>;
+};
+
+export default BannerSetting;
